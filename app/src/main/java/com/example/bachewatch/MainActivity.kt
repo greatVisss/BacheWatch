155c3ae9070b5b64d90ad6395247197c42ab6eba
Jesus Edu <jesuseduardotv24@gmail.com>
2026-06-12 23:26:08 -0600

    3

--- app/src/main/java/com/example/bachewatch/MainActivity.kt
+++ app/src/main/java/com/example/bachewatch/MainActivity.kt
@@ -28,6 +28,7 @@ import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.android.gms.maps.model.CameraPosition
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
