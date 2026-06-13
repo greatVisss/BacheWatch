cc71bfe2d07cb0b16388b12596413cbba77a1056
Jesus Edu <jesuseduardotv24@gmail.com>
2026-06-12 23:55:26 -0600

    4

--- app/src/main/java/com/example/bachewatch/MainActivity.kt
+++ app/src/main/java/com/example/bachewatch/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.gestures.snapping.SnapPosition.Center.position
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -18,8 +17,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bachewatch.ui.theme.BacheWatchTheme
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.GoogleMapComposable
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+
 import com.example.bachewatch.BachesRepository
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
